8f59613029ca12d96a9fa6f46d6cc05d1842d47c
Vyacheslav Tverskoy <tsx@tsx.su>
2017-12-29 17:03:45 +0500

    Improved redis scan script

--- scripts/redis/scan_redis.py
+++ scripts/redis/scan_redis.py
@@ -1,4 +1,5 @@
-"""Safely scan entire Redis instance and report key stats."""
+"""Safely scan entire Redis instance and report key sizes."""
+import itertools
 import signal
 import time
 
@@ -10,6 +11,9 @@ PORT = 6379
 DB = 0
 
 DELAY = .01  # Sleep a little to not impact redis performance
+KEYS_PER_BATCH = 10  # How much keys to query in a single iteration
+
+FETCH_TYPE_TTL = True  # Set this to False to make it fetch less data / go faster
 
 loop = True
 
@@ -22,22 +26,17 @@ def signal_handler(signum, frame):
     loop = False
 
 
-def get_size(client, key, key_type):
-    """Get size of key."""
+def grouper(iterable, size):
+    """Group items from iterable into sequences of fixed size.
 
-    size = -1
-    if key_type == 'string':
-        size = client.strlen(key)
-    elif key_type == 'zset':
-        size = client.zcard(key)
-    elif key_type == 'set':
-        size = client.scard(key)
-    elif key_type == 'list':
-        size = client.llen(key)
-    elif key_type == 'hash':
-        size = client.hlen(key)
-
-    return size
+    >>> list(grouper([1, 2, 3, 4, 5], 2))
+    [[1, 2], [3, 4], [5]]
+    """
+    it = iter(iterable)
+    item = list(itertools.islice(it, size))
+    while item:
+        yield item
+        item = list(itertools.islice(it, size))
 
 
 def run():
@@ -46,26 +45,28 @@ def run():
     client = redis.Redis(host=HOSTNAME, port=PORT, db=DB)
 
     print 'Scanning redis keys'
-    cursor = '0'
     match = None
-
-    log_file = file('redis-stats.log', 'w')
-
     signal.signal(signal.SIGINT, signal_handler)
 
-    while cursor != 0 and loop:
+    with file('redis-stats.log', 'w') as log_file:
+        for keys in grouper(client.scan_iter(match=match), KEYS_PER_BATCH):
+            if not loop:
+                break
 
-        cursor, data = client.scan(cursor=cursor, match=match)
-
-        for key in data:
-            key_type = client.type(key)
-            size = get_size(client, key, key_type)
-            ttl = client.ttl(key)
-            log_file.write('%s %s %s %d\n' % (key, key_type, str(ttl), size))
-        log_file.flush()
-        time.sleep(DELAY)
-
-    log_file.close()
+            pipe = client.pipeline()
+            for key in keys:
+                pipe.debug_object(key)
+                if FETCH_TYPE_TTL:
+                    pipe.type(key)
+                    pipe.ttl(key)
+            if FETCH_TYPE_TTL:
+                for key, (r, t, ttl) in zip(keys, grouper(pipe.execute(), 3)):
+                    log_file.write('%s %s %s %d\n' % (key, t, ttl, r['serializedlength']))
+            else:
+                for key, r in zip(keys, pipe.execute()):
+                    log_file.write('%s %d\n' % (key, r['serializedlength']))
+            log_file.flush()
+            time.sleep(DELAY)
 
 
 if __name__ == '__main__':
