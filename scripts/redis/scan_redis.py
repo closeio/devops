06077caa88cba2f78307d93758710b6b043835fe
Joe Kemp <joe@close.io>
2019-11-01 13:51:04 -0400

    Fix up

--- scripts/redis/scan_redis.py
+++ scripts/redis/scan_redis.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
-"""Safely scan entire Redis instance and report key stats."""
+"""
+Safely scan Redis instance and report key stats.
+
+Can also set the TTL for keys to facilitate removing data.
+"""
 from __future__ import absolute_import, print_function
 
 import signal
+import sys
 import time
 
 import click
@@ -56,6 +61,10 @@ def get_size(client, key, key_type):
 def run(host, port, db, delay, file_name, print_it, match, set_ttl=None):
     """Run scan."""
 
+    if set_ttl is not None and match is None:
+        print('You must specify match when setting TTLs!')
+        sys.exit(1)
+
     client = redis.Redis(host=host, port=port, db=db)
 
     print('Scanning redis keys with match: %s\n' % match)
@@ -73,15 +82,17 @@ def run(host, port, db, delay, file_name, print_it, match, set_ttl=None):
             key_type = client.type(key)
             size = get_size(client, key, key_type)
 
-            # ttl() returns None in redis 2.x and -1 in redis 3.x for
-            # keys that don't have an expiration.
             ttl = client.ttl(key)
+            # ttl() returns None in redis 2.x and -1 in redis 3.x for
+            # keys that don't have an expiration. Normalize it here.
+            if ttl is None:
+                ttl = -1
 
             new_ttl = None
             if set_ttl == -1:
                 client.persist(key)
                 new_ttl = -1
-            elif set_ttl is not None and (ttl == -1 or ttl is None):
+            elif set_ttl is not None and ttl == -1:
                 # Only change TTLs for keys with no TTL
                 client.expire(key, set_ttl)
                 new_ttl = set_ttl
